f9b4a3ac51abe34b496ab220a489c30e6215c10f
alexseleznevbot <alex.seleznev.bot@gmail.com>
2026-02-03 11:56:45 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Calendar, Clock, CheckCircle, XCircle, Plus, Trash2, ChevronLeft, ChevronRight, Phone, ArrowLeft, X, History, AlertCircle, List, Users, Send } from 'lucide-react';
 
 // API Configuration
-const API_URL = 'https://script.google.com/macros/s/AKfycbwp3-LW4GeUVzMO4Bc-Bdca39SUVeRfViNoSVWIRD1Q5Y54T96hIhtxJ58AOnmIhjGlPg/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbxu5Jkw1R6hHNwBeMG024Ey6P0bUKW4HU78h9INwr1inVGkzMY6it6atL_6BXuD-OzbRw/exec';
 const ADMIN_SECRET = 'ShsHockey_2026_!Seleznev';
 
 // Hockey puck logo
